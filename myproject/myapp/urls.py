24059c8c066aaa3c846e7c93e33cc3e2c0eaf01d
sunantha <sunantha2348@gmail.com>
2025-05-11 03:32:33 +0700

    Remove unnecessary files

--- myproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, StoreListView, StoreDetailView, cart_view
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('stores/', StoreListView.as_view(), name='store_list'),
-    path('stores/<uuid:pk>/', StoreDetailView.as_view(), name='store_detail'),
-    path('cart/', cart_view, name='cart'),
-]
