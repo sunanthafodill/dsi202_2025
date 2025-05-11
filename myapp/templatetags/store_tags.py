b6f1113f1b307333e6899a08f48097aa8fe372ff
sunanthafodill <158834410+sunanthafodill@users.noreply.github.com>
2025-05-11 22:43:54 +0700

    Delete myapp directory

--- myapp/templatetags/store_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def split(value, delimiter):
-    if value:
-        return [item.strip() for item in value.split(delimiter)]
-    return []
